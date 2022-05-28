b0f35a03b508980cf0a87ac63385abd872156ed6
Uros Stankovic <urosboy2000@gmail.com>
2022-05-28 13:27:04 +0200

    Added maven framework and dependency for bouncy castle lib

--- src/main/java/etf/openpgp/su182095dvv180421d/MainFrame.java
+++ src/main/java/etf/openpgp/su182095dvv180421d/MainFrame.java
@@ -1,5 +1,7 @@
 package etf.openpgp.su182095dvv180421d;
 
+import etf.openpgp.su182095dvv180421d.views.PublicKeyRingView;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,9 +12,9 @@ public class MainFrame extends JFrame {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         JTabbedPane jTabbedPane = new JTabbedPane();
+
         mainFrame.add(jTabbedPane);
         mainFrame.setTitle("Zastita podatak - Open PGP");
-        mainFrame.setSize(640, 480);
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         mainFrame.setBounds(
                 size.width / 2 - Config.FRAME_WIDTH / 2,
@@ -20,6 +22,9 @@ public class MainFrame extends JFrame {
                 Config.FRAME_WIDTH,
                 Config.FRAME_HEIGHT);
         mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        jTabbedPane.addTab("Pregled prstena javnih kljuceva", new PublicKeyRingView());
+
         mainFrame.setVisible(true);
     }
 
---
+++ src/main/java/etf/openpgp/su182095dvv180421d/views/PublicKeyRingView.java
@@ -0,0 +1,6 @@
+package etf.openpgp.su182095dvv180421d.views;
+
+import javax.swing.*;
+
+public class PublicKeyRingView extends JPanel {
+}
