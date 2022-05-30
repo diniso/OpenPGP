b494c90db4b1ec6fc3767b598103abe0991dacf1
Uros Stankovic <urosboy2000@gmail.com>
2022-05-30 20:52:40 +0200

    Changed height

--- src/main/java/etf/openpgp/su182095dvv180421d/Config.java
+++ src/main/java/etf/openpgp/su182095dvv180421d/Config.java
@@ -1,7 +1,7 @@
 package etf.openpgp.su182095dvv180421d;
 
 public class Config {
-    public static final int FRAME_WIDTH = 640;
+    public static final int FRAME_WIDTH = 900;
     public static final int FRAME_HEIGHT = 480;
 
     public static final String privateKeyRingFile = "";
