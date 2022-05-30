13cf9a243543eee767d36d4f63c25c99ea660191
Uros Stankovic <urosboy2000@gmail.com>
2022-05-30 20:54:18 +0200

    Removed encrypted private key field from preview of the private key ring.

--- src/main/java/etf/openpgp/su182095dvv180421d/views/PrivateKeyRingView.java
+++ src/main/java/etf/openpgp/su182095dvv180421d/views/PrivateKeyRingView.java
@@ -1,27 +1,20 @@
 package etf.openpgp.su182095dvv180421d.views;
 
-import etf.openpgp.su182095dvv180421d.Config;
 import etf.openpgp.su182095dvv180421d.model.Observer;
 import etf.openpgp.su182095dvv180421d.model.PrivateKeyRing;
 import etf.openpgp.su182095dvv180421d.model.Utils;
-import org.bouncycastle.openpgp.PGPException;
-import org.bouncycastle.openpgp.PGPPrivateKey;
 import org.bouncycastle.openpgp.PGPSecretKey;
-import org.bouncycastle.openpgp.PGPSecretKeyRing;
-import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
 import org.bouncycastle.util.encoders.Base64;
 
 import javax.swing.*;
-import java.awt.*;
-import java.math.BigInteger;
-import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.List;
 
 public class PrivateKeyRingView extends JPanel implements Observer<List<PGPSecretKey>> {
 
     private JTable table;
-    private JScrollPane sp;
+    private final JScrollPane sp;
 
     public PrivateKeyRingView() {
         this.setOpaque(false);
@@ -38,19 +31,18 @@ public class PrivateKeyRingView extends JPanel implements Observer<List<PGPSecre
     public void setData(List<PGPSecretKey> pks) {
         if (table != null) this.sp.remove(table);
 
-        String data[][] = new String[pks.size()][5];
-        String column[] = {"Timestamp", "Key ID", "Public key", "Ecrypted private key", "User Id"};
+        String[][] data = new String[pks.size()][4];
+        String[] column = {"Timestamp", "Key ID", "Public key", "User Id"};
         for (int i = 0; i < pks.size(); i++) {
             PGPSecretKey pk = pks.get(i);
-            data[i][0] = pk.getPublicKey().getCreationTime().toString();
+
+            data[i][0] = new SimpleDateFormat("yyyy-MM-dd hh:mm").format(pk.getPublicKey().getCreationTime());
             data[i][1] = Utils.getPGPPrivateKeyIdBase64(pk);
             data[i][2] = Base64.toBase64String(pk.getPublicKey().getPublicKeyPacket().getKey().getEncoded());
-            // TODO: Change it to real encrypted private key
-            data[i][3] = "ABCFEGH";
-            data[i][4] = "";
+            data[i][3] = "";
             Iterator<String> userIDs = pk.getUserIDs();
             if (userIDs.hasNext()) {
-                data[i][4] = userIDs.next();
+                data[i][3] = userIDs.next();
             }
         }
 
@@ -61,9 +53,7 @@ public class PrivateKeyRingView extends JPanel implements Observer<List<PGPSecre
         this.sp.setViewportView(table);
 
         table.setFillsViewportHeight(true);
-
     }
-
 
     @Override
     public void observableChanged(List<PGPSecretKey> pks) {
